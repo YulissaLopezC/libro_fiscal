d7d211e530fccc456f9f670d9c38718eafd818ef
Yulissa Lopez Cortes <lyulissa5@gmail.com>
2026-03-28 17:05:23 -0500

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDL0yLfwYChFEpxrU6JME2O2lumZSCPXL0",
-  authDomain: "libro-fiscal-121c9.firebaseapp.com",
-  projectId: "libro-fiscal-121c9",
-  storageBucket: "libro-fiscal-121c9.firebasestorage.app",
-  messagingSenderId: "39139916971",
-  appId: "1:39139916971:web:6bf23ae349a5d5fe87cbee"
+  apiKey: "AIzaSy***************",
+  authDomain: "tu-proyecto.firebaseapp.com",
+  projectId: "tu-proyecto-id",
+  storageBucket: "tu-proyecto.appspot.com",
+  messagingSenderId: "***********",
+  appId: "1:***********:web:***************"
 };
 
 const app = initializeApp(firebaseConfig);
